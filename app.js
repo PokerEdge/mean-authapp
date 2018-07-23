d700b3bce3df05f7b9e6b214d93a76970cbcda85
Reid <Reid@pokersprout.com>
2018-07-22 23:20:42 -0400

    Route scaffolding for users routes and 'public' directory defined
    Route scaffolding for users routes and 'public' directory defined. Now
    ready to connect to Mongo DB.

--- app.js
+++ app.js
@@ -7,9 +7,24 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+// Import all the user routes from another file
+const users = require('./routes/users');
+
 const port = 3000;
 
-// Goes to front end index file
+// CORS module allows us to make app public while authentication limits app access
+app.use(cors());
+
+// Set static folder for public files (client)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware - Parses incoming request body so it can be grabbed (as JSON)
+app.use(bodyParser.json());
+
+// Any route within ./routes/users is placed after '/users'
+app.use('/users', users);
+
+// Goes to front end index file, index route
 app.get('/', (req, res) => {
     res.send('INVALID ENDPOINT');
 });
